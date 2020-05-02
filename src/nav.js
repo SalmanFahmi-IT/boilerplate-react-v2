df939e73640cd53610b555b7f9aa3546c0110b21
Fahmi Salman <sfahmi@pca.ma>
2020-05-02 14:35:51 +0000

    feat: add nav items list

---
+++ src/nav.js
@@ -0,0 +1,13 @@
+const navigation = [
+	{ name: 'Dashboard', url: '/dashboard', icon: 'dashboard' },
+	{ name: 'Portefeuille clients', url: '/clients', icon: 'customer-portfolio' },
+	{ name: 'Proposition commerciales', url: '/propositions', icon: 'list-proposal' },
+	{ name: 'Contrats', url: '/contrats', icon: 'contrat' },
+	{ name: 'Mes bons commandes', url: '/commandes', icon: 'icon5' },
+	{ name: 'Mes réglements', url: '/reglements', icon: 'icon6' },
+	{ name: 'Mes immatriculations', url: '/immatriculations', icon: 'icon7' },
+	{ name: 'Offres et Produits', url: '/offers', icon: 'shop' },
+	{ name: 'Nouvelle proposition', url: '/newproposition', icon: 'shop' },
+];
+
+export default navigation;
